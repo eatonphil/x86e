7b01f404105e3be02c0006a5b69951d243b3226e
Phil Eaton <philneaton95@gmail.com>
2019-04-22 22:40:37 -0400

    Add fib example

---
+++ examples/fib.c
@@ -0,0 +1,13 @@
+int fibonacci(int n) {
+  int a, b = 0, current = 1;
+
+  for (int i = 1; i < n; i++) {
+    a = b;
+    b = current;
+    current = a + b;
+  }
+
+  return current;
+}
+
+int main() { return fibonacci(5); }
